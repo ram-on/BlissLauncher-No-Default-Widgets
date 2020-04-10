32b6a18ab6cfe74424c42d786637a8858e0f4e92
Amit Kumar <amitkma@e.email>
2020-04-10 21:43:00 +0530

    Fix spotless violation

--- domain/src/main/java/foundation/e/blisslauncher/domain/inject/DomainComponent.kt
+++ domain/src/main/java/foundation/e/blisslauncher/domain/inject/DomainComponent.kt
@@ -2,8 +2,6 @@ package foundation.e.blisslauncher.domain.inject
 
 import foundation.e.blisslauncher.common.compat.LauncherAppsCompat
 import foundation.e.blisslauncher.common.executors.AppExecutors
-import foundation.e.blisslauncher.domain.manager.LauncherStateManager
-import foundation.e.blisslauncher.domain.repository.LauncherItemRepository
 
 /**
  * Interface that lists all public repositories and data access layer components which are needed
