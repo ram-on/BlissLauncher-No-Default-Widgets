c80e4ff079848ca5ac8994e8c4eab134ca33639c
Amit Kumar <amitkma@e.email>
2020-04-10 21:15:56 +0530

    Delete test entites as of now

--- data/src/main/java/foundation/e/blisslauncher/data/inject/DataRepoBindingModule.kt
+++ data/src/main/java/foundation/e/blisslauncher/data/inject/DataRepoBindingModule.kt
@@ -3,17 +3,8 @@ package foundation.e.blisslauncher.data.inject
 import dagger.Module
 import dagger.Provides
 import foundation.e.blisslauncher.data.LauncherStateManagerImpl
-import foundation.e.blisslauncher.data.apps.AppsRepositoryImpl
-import foundation.e.blisslauncher.data.launcher.LauncherRepositoryImpl
-import foundation.e.blisslauncher.data.shortcuts.ShortcutsRepositoryImpl
-import foundation.e.blisslauncher.data.widgets.WidgetsRepositoryImpl
-import foundation.e.blisslauncher.data.workspace.WorkspaceRepositoryImpl
 import foundation.e.blisslauncher.domain.manager.LauncherStateManager
-import foundation.e.blisslauncher.domain.repository.AppsRepository
 import foundation.e.blisslauncher.domain.repository.LauncherItemRepository
-import foundation.e.blisslauncher.domain.repository.ShortcutRepository
-import foundation.e.blisslauncher.domain.repository.WidgetsRepository
-import foundation.e.blisslauncher.domain.repository.WorkspaceRepository
 
 @Module
 class DataRepoBindingModule {
@@ -22,17 +13,5 @@ class DataRepoBindingModule {
     fun bindLauncherStateManager(launcherStateManagerImpl: LauncherStateManagerImpl): LauncherStateManager = launcherStateManagerImpl
 
     @Provides
-    fun bindAppsRepository(appsRepositoryImpl: AppsRepositoryImpl): AppsRepository = appsRepositoryImpl
-
-    @Provides
-    fun bindShortcutRepository(shortcutRepositoryImpl: ShortcutsRepositoryImpl): ShortcutRepository = shortcutRepositoryImpl
-
-    @Provides
-    fun bindLauncherRepository(launcherRepositoryImpl: LauncherRepositoryImpl): LauncherItemRepository = launcherRepositoryImpl
-
-    @Provides
-    fun bindWorkspaceRepository(workspaceRepositoryImpl: WorkspaceRepositoryImpl): WorkspaceRepository = workspaceRepositoryImpl
-
-    @Provides
-    fun bindWidgetsRepository(widgetsRepositoryImpl: WidgetsRepositoryImpl): WidgetsRepository = widgetsRepositoryImpl
+    fun bindLauncherRepository(launcherRepositoryImpl: LauncherItemRepository): LauncherItemRepository = launcherRepositoryImpl
 }
