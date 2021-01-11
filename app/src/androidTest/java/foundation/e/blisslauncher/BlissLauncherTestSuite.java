927c46fb66943d74ce1dd65ea20c3e72c19b68b8
vincent Bourgmayer <vincent.bourgmayer@e.email>
2021-01-11 16:31:48 +0100

    update BlissLauncherTestSuite.java

--- app/src/androidTest/java/foundation/e/blisslauncher/BlissLauncherTestSuite.java
+++ app/src/androidTest/java/foundation/e/blisslauncher/BlissLauncherTestSuite.java
@@ -1,11 +1,13 @@
 package foundation.e.blisslauncher;
 
-
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
-@RunWith(Suite::class
-@Suite.Classes(
+
+import foundation.e.blisslauncher.features.launcher.LauncherActivityUiTest;
+
+@RunWith(Suite::class)
+@Suite.SuiteClasses(
 /* Here list all you tests classes that are part of the suite
 * eg: 
 * mainActivityTest::class,     * secondActivityTest::class,
