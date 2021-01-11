b882153b733ec1eb68526d94810de9afce3ce8ac
Vincent Bourgmayer <vincent.bourgmayer@eelo.io>
2021-01-11 14:46:48 +0000

    Add BlissLauncherTestSuite

---
+++ app/src/androidTest/java/foundation/e/blisslauncher/BlissLauncherTestSuite.java
@@ -0,0 +1,9 @@
+ @RunWith(Suite::class
+ @Suite.Classes(
+ /* Here list all you tests classes that are part of the suite
+ * eg: 
+ * mainActivityTest::class,     * secondActivityTest::class,
+ * serviceTest::class
+ */
+ )
+ class BlissLauncherTestSuite
