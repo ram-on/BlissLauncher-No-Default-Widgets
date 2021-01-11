025aaa5343e3deae0ac4abd8b59cb676bd43e225
Vincent Bourgmayer <vincent.bourgmayer@eelo.io>
2021-01-11 14:54:52 +0000

    add import & package lines. Add LauncherActivityUiTest as test to run in BlissLauncherTestSuite.java

--- app/src/androidTest/java/foundation/e/blisslauncher/BlissLauncherTestSuite.java
+++ app/src/androidTest/java/foundation/e/blisslauncher/BlissLauncherTestSuite.java
@@ -1,9 +1,17 @@
- @RunWith(Suite::class
- @Suite.Classes(
- /* Here list all you tests classes that are part of the suite
- * eg: 
- * mainActivityTest::class,     * secondActivityTest::class,
- * serviceTest::class
- */
- )
- class BlissLauncherTestSuite
+package foundation.e.blisslauncher;
+
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import foundation.e.blisslauncher.features.launcher;
+
+@RunWith(Suite::class
+@Suite.Classes(
+/* Here list all you tests classes that are part of the suite
+* eg: 
+* mainActivityTest::class,     * secondActivityTest::class,
+* serviceTest::class
+*/
+    LauncherActivityUiTest::class
+)
+class BlissLauncherTestSuite
